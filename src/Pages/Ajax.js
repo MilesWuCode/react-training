e6c65ed81cf8af198f135aac5a6c280b954cf563
Miles Wu <slion0725@gmail.com>
2020-07-30 12:01:58 +0800

    u

--- src/Pages/Ajax.js
+++ src/Pages/Ajax.js
@@ -13,7 +13,8 @@ class Ajax extends React.Component {
   }
 
   componentDidMount() {
-    postApi.all()
+    postApi
+      .all()
       .then(({ data }) => {
         console.log(data);
       })
@@ -26,6 +27,92 @@ class Ajax extends React.Component {
     return (
       <div>
         <h3 className="text-2xl">Ajax</h3>
+        <div className="w-full max-w-screen-xl px-6 mx-auto">
+          <div className="flex justify-center p-4 px-3 py-10">
+            <div className="w-full max-w-md">
+              <div className="px-3 py-2 mb-4 bg-white rounded-lg shadow-md">
+                <div className="block px-2 py-2 text-lg font-semibold text-gray-700">
+                  Item List
+                </div>
+                <div className="flex items-center bg-gray-200 rounded-md">
+                  <div className="pl-2">
+                    <svg
+                      className="w-6 h-6 text-gray-500 fill-current"
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 24 24"
+                    >
+                      <path
+                        className="heroicon-ui"
+                        d="M16.32 14.9l5.39 5.4a1 1 0 0 1-1.42 1.4l-5.38-5.38a8 8 0 1 1 1.41-1.41zM10 16a6 6 0 1 0 0-12 6 6 0 0 0 0 12z"
+                      />
+                    </svg>
+                  </div>
+                  <input
+                    className="w-full px-2 py-2 leading-tight text-gray-700 bg-gray-200 rounded-md focus:outline-none"
+                    id="search"
+                    type="text"
+                    placeholder="Search teams or members"
+                  />
+                </div>
+                <div className="py-3 text-sm">
+                  <div className="flex justify-start px-2 py-2 my-2 text-gray-700 rounded-md cursor-pointer hover:text-blue-400 hover:bg-blue-100">
+                    <span className="w-2 h-2 m-2 bg-gray-400 rounded-full"></span>
+                    <div className="flex-grow px-2 font-medium">
+                      Tighten Co.
+                    </div>
+                    <div className="text-sm font-normal tracking-wide text-gray-500">
+                      Team
+                    </div>
+                  </div>
+                  <div className="flex justify-start px-2 py-2 my-2 text-gray-700 rounded-md cursor-pointer hover:text-blue-400 hover:bg-blue-100">
+                    <span className="w-2 h-2 m-2 bg-green-400 rounded-full"></span>
+                    <div className="flex-grow px-2 font-medium">
+                      Taylor Otwell
+                    </div>
+                    <div className="text-sm font-normal tracking-wide text-gray-500">
+                      Member
+                    </div>
+                  </div>
+                  <div className="flex justify-start px-2 py-2 my-2 text-gray-700 rounded-md cursor-pointer hover:text-blue-400 hover:bg-blue-100">
+                    <span className="w-2 h-2 m-2 bg-gray-400 rounded-full"></span>
+                    <div className="flex-grow px-2 font-medium">
+                      Adam Wathan
+                    </div>
+                    <div className="text-sm font-normal tracking-wide text-gray-500">
+                      Member
+                    </div>
+                  </div>
+                  <div className="flex justify-start px-2 py-2 my-2 text-gray-700 rounded-md cursor-pointer hover:text-blue-400 hover:bg-blue-100">
+                    <span className="w-2 h-2 m-2 bg-gray-400 rounded-full"></span>
+                    <div className="flex-grow px-2 font-medium">
+                      Duke Street Studio Inc.
+                    </div>
+                    <div className="text-sm font-normal tracking-wide text-gray-500">
+                      Team
+                    </div>
+                  </div>
+                  <div className="flex justify-start px-2 py-2 my-2 text-gray-700 rounded-md cursor-pointer hover:text-blue-400 hover:bg-blue-100">
+                    <span className="w-2 h-2 m-2 bg-green-400 rounded-full"></span>
+                    <div className="flex-grow px-2 font-medium">
+                      Jeffrey Wey
+                    </div>
+                    <div className="text-sm font-normal tracking-wide text-gray-500">
+                      Member
+                    </div>
+                  </div>
+                </div>
+                <div className="block px-3 py-2 -mx-3 -mb-2 text-sm text-right bg-gray-200 rounded-b-lg">
+                  <button className="px-4 py-2 font-bold text-gray-500 hover:text-gray-600">
+                    Cancel
+                  </button>
+                  <button className="px-4 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-700">
+                    Invite
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
